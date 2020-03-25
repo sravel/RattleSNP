14f0b5f54b4938779611394411ea1fe6372ac10d
sravel <sebastien.ravel@cirad.fr>
2020-03-25 09:30:38 +0100

    add  to add rerun or other snake option

--- Launcher.sh
+++ Launcher.sh
@@ -15,14 +15,14 @@ datas_config=`pwd`"/config.yaml"
 #snakemake --configfile ${datas_config} --cluster-config ${cluster_config}  --dag > schema_pipeline_samples.dot && dot schema_pipeline_samples.dot -Tpdf > schema_pipeline_samples.pdf
 #exit
 
-snakemake --latency-wait 5184000 --jobs 100 --cluster "qsub {cluster.queue} {cluster.export_env} {cluster.cwd} {cluster.mem} {cluster.n_cpu} {cluster.logerror} {cluster.log} " --cluster-config ${cluster_config}  --configfile ${datas_config}
+snakemake --latency-wait 5184000 --jobs 100 --cluster "qsub {cluster.queue} {cluster.export_env} {cluster.cwd} {cluster.mem} {cluster.n_cpu} {cluster.logerror} {cluster.log} " --cluster-config ${cluster_config}  --configfile ${datas_config} $@
 
 #snakemake --shadow-prefix ${scratch_dir} --latency-wait 5184000 --jobs 100 --drmaa "{cluster.queue} {cluster.export_env} {cluster.cwd} {cluster.mem} {cluster.n_cpu}{threads} {cluster.logerror}{params.errorLog} {cluster.log}{params.outputLog} " --cluster-config ${cluster_config}  --configfile ${datas_config}
 
 
 #snakemake --configfile ${datas_config} --cluster-config ${cluster_config}  --filegraph | dot -Tpdf > schema_pipeline_files.pdf
 #snakemake --configfile ${datas_config} --cluster-config ${cluster_config}  --dag | dot -Tpdf > schema_pipeline_samples.pdf
-snakemake --report REPORT.html
+#snakemake --report REPORT.html
 
 ## add to clean files
 ##snakemake -s sRNA_pipeline.snake clean
