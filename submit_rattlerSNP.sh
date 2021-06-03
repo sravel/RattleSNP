ad571abd26161c5ad968529417a22c46b45d1014
sravel <sebastien.ravel@cirad.fr>
2021-06-03 09:39:57 +0200

    add cluster info

---
+++ submit_rattlerSNP.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#SBATCH --job-name rattlerSNP
+#SBATCH --output slurm-%x_%j.log
+#SBATCH --error slurm-%x_%j.log
+#SBATCH --partition=long
+
+config="None"
+# module help
+function help
+{
+    printf "\033[36m####################################################################\n";
+    printf "#                     submit_rattlerSNP                             #\n";
+    printf "####################################################################\n";
+    printf "
+ Input:
+    Configuration file for snakemake
+
+ Exemple Usage: ./submit_rattlerSNP.sh -c config.yaml -k cluster_config.yaml
+
+ Usage: ./submit_rattlerSNP.sh -c {file} -k {file}
+    options:
+        -c {file} = Configuration file for run rattlerSNP
+        -k {file} = Configuration file cluster for run rattlerSNP
+
+        -h = see help\n\n"
+    exit 0
+}
+
+
+##################################################
+## Parse command line options.
+while getopts c:k:h: OPT;
+    do case $OPT in
+        c)    config=$OPTARG;;
+        k)    cluster_config=$OPTARG;;
+        h)    help;;
+        \?)    help;;
+    esac
+done
+
+if [ $# -eq 0 ]; then
+    help
+fi
+
+##################################################
+## Main code
+##################################################
+
+if [ ! -z "$config" ] && [ -e $config ]; then
+
+    config=`readlink -m $config`
+    echo "CONFIG FILE IS $config"
+    # SLURM JOBS PROFILES
+    if [ ! -z "$cluster_config" ] && [ -e $cluster_config ]; then
+      cluster_config=`readlink -m $cluster_config`
+      echo "CLUSTER CONFIG FILE IS $cluster_config"
+    snakemake --cores -p -s $RATTLERSNP/Snakefile \
+    --configfile $config \
+    --cluster-config $cluster_config \
+    --profile /shared/home/jcarlier/.config/snakemake/rattleSNP
+  else
+    snakemake --cores -p -s $RATTLERSNP/Snakefile \
+    --configfile $config \
+    --profile /shared/home/jcarlier/.config/snakemake/rattleSNP
+  fi
+else
+    echo "configfile = "$config
+    echo "cluster_config = "$cluster_config
+    printf "\033[31m \n\n You must add a valid config file !!!!!!!!!!!! \n\n"
+    help
+fi
