54e863fe7d88e68490ee5b81cc51c1d9a8066db1
ravel <sebastien.ravel@cirad.fr>
2022-03-17 16:12:58 +0100

    remove pysamstats

--- rattleSNP/snakemake_scripts/rattleSNP_module.py
+++ rattleSNP/snakemake_scripts/rattleSNP_module.py
@@ -25,9 +25,8 @@ def parse_idxstats(files_list=None, out_csv=None, sep="\t"):
         dataframe_mapping_stats.to_csv(out_csv_file, index=True, sep=sep)
 
 
-def check_mapping_stats(bam, out_csv, sep="\t"):
+def check_mapping_stats(bam, depth_file, out_csv, sep="\t"):
     from numpy import median, mean
-    from pysamstats import load_coverage
     import pysam
     import re
     import pandas as pd
@@ -36,19 +35,19 @@ def check_mapping_stats(bam, out_csv, sep="\t"):
     # for bam in bam_files:
     if not Path(bam+"bai").exists(): pysam.index(Path(bam).as_posix())
     sample = Path(bam).stem
-    # print(f"\n\n{'*'*30}\nSAMPLE NAME: {sample}\n{'*'*30}\n\n")
+    listMap = []
+    with open(depth_file, "r") as depth_file_open:
+        for line in depth_file_open:
+            depth = line.rstrip().split("\t")[-1]
+            listMap.append(int(depth))
     bam_file = pysam.AlignmentFile(bam, "r")
     name_fasta_ref = Path(re.findall("[/].*\.fasta",bam_file.header["PG"][0]["CL"], flags=re.IGNORECASE)[0]).stem
     if name_fasta_ref not in dico_size_ref_genome:
         dico_size_ref_genome[name_fasta_ref] = sum([dico["LN"] for dico in bam_file.header["SQ"]])
-    a = load_coverage(bam_file, pad=True)
-    df = pd.DataFrame(a)
-    df.chrom = df.chrom.str.decode(encoding = 'UTF-8')
-    listMap = df[df.reads_all >= 1].reads_all
-
-    dicoResume[sample]["Mean mapping Depth coverage"] = f"{mean(listMap):.2f}"
-    dicoResume[sample]["Median mapping Depth coverage"] = f"{median(listMap):.2f}"
-    dicoResume[sample]["Mean Genome Coverage"] = f"{(len(listMap)/dico_size_ref_genome[name_fasta_ref])*100:.2f}%"
+    dicoResume[sample]["Mean Depth"] = f"{mean(listMap):.2f}X"
+    dicoResume[sample]["Median Depth"] = f"{median(listMap):.2f}X"
+    dicoResume[sample]["Max Depth"] = f"{max(listMap):.2f}X"
+    dicoResume[sample]["Mean Genome coverage"] = f"{(len(listMap)/dico_size_ref_genome[name_fasta_ref])*100:.2f}%"
 
     dataframe_mapping_stats = pd.DataFrame.from_dict(dicoResume, orient='index')
     with open(out_csv, "w") as out_csv_file:
@@ -63,7 +62,7 @@ def merge_bam_stats_csv(csv_files, csv_file, sep="\t"):
     df = pd.concat(df)
     df.rename(columns={'Unnamed: 0':'Samples'}, inplace=True)
     with open(csv_file, "w") as libsizeFile:
-        print(f"All CSV infos:\n{df}\n")
+        # print(f"All CSV infos:\n{df}\n")
         df.to_csv(libsizeFile, index=False, sep=sep)
 
 
